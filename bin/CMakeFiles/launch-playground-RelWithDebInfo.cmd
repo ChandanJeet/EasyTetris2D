051e6339b0fa9d72137d950e8d9251a2dd16990c
ChandanJeet <chndnjeet@gmail.com>
2021-11-12 17:11:43 +0100

    Bin folder

---
+++ bin/CMakeFiles/launch-playground-RelWithDebInfo.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/chje272771/Documents/_Lectures/Winter Semester 2021-2022/CG/src/playground/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\chje272771\Documents\_Lectures\Winter Semester 2021-2022\CG\bin\RelWithDebInfo\playground.exe"  
+pause
